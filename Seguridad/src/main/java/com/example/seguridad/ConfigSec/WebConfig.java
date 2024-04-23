c5cc8e31468ee3477697265d7a55d29142acbd79
Abraham-Rendon-2003 <105471973+Abraham-Rendon-2003@users.noreply.github.com>
2024-04-23 12:32:47 -0600

    Seguridad

---
+++ Seguridad/src/main/java/com/example/seguridad/ConfigSec/WebConfig.java
@@ -0,0 +1,19 @@
+package com.example.seguridad.ConfigSec;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@EnableWebMvc
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry){
+        registry.addViewController("/").setViewName("index");
+        registry.addViewController("/index").setViewName("index");
+        registry.addViewController("/home").setViewName("home");
+        registry.addViewController("/form").setViewName("form");
+        registry.addViewController("/login").setViewName("login");
+    }
+}
